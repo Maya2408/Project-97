028e33bbb63c4968693b1898039815b8733e2298
Maya2408 <84226035+Maya2408@users.noreply.github.com>
2022-06-05 09:16:57 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,15 @@
+import random
+
+print ("Number guessing game")
+number = random.randint(1, 9)
+
+chance = 0
+guess=int(input("Make a guess: "))
+while (chance < 5):
+    print (guess)
+
+if chance == number:
+    print("Great guess! You won!")
+    
+if not chance < 5:
+    print("The number was", number, "Better luck next time!")
